17db17c7d6509bb633690777a4cd2765cd0529ef
solit <s>
2021-10-06 21:08:54 +0700

    test 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,10 @@ public class Main {
         //1: Số lượng số 0 ở đầu phải nhiều hơn hoặc = với target
         //2: DecimalData của hash phải nhỏ hơn hoặc = DecimalData của target
         System.out.println("max target: " + new BigDecimal(new BigInteger("00000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16)));
-        int bitDoKho = 386803250;
+        int bitTarget = 386803250;
         String hashCheck = "0000000000000000000193d0666a165edb6dacdc64fdeb6f752a13e532c5509e";
 
-        String hex = Integer.toHexString(bitDoKho);
+        String hex = Integer.toHexString(bitTarget);
         int index = Integer.parseInt(hex.substring(0, 2), 16);
         int number = Integer.parseInt(hex.substring(2, hex.length()), 16);
 
@@ -27,6 +27,5 @@ public class Main {
         // 1 là lớn hơn
         System.out.println("Độ khó targer và hashData là :" + (currentTarget.compareTo(new BigDecimal(new BigInteger(hashCheck, 16))) >= 0));
         System.out.println(new BigDecimal(new BigInteger(hashCheck, 16)));
-        System.out.println(currentTarget.divide(new BigDecimal(new BigInteger(hashCheck, 16))));
     }
 }
